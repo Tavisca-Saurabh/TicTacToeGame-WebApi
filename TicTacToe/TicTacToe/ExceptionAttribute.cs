e5d80b0c5d761237277924e9dd3c1612e1077744
Saurabh Manchanda <smanchanda@tavisca.com>
2018-09-13 21:22:49 +0530

    TicTacToe Game WebApi

---
+++ TicTacToe/TicTacToe/ExceptionAttribute.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TicTacToe.DataAccess;
+using TicTacToe.Model;
+
+namespace TicTacToe
+{
+    public class ExceptionAttribute : ExceptionFilterAttribute
+    {
+        Log logObject = new Log();
+        LogData addingobject = new LogData();
+
+        public override void OnException(ExceptionContext actionExecutedContext)
+        {
+            if (actionExecutedContext.Exception is Exception)
+            {
+                logObject.Request = actionExecutedContext.RouteData.Values["action"].ToString() + " " + actionExecutedContext.RouteData.Values["action"].ToString();
+                logObject.Exception = actionExecutedContext.Exception.ToString();
+                var index = logObject.Exception.IndexOf("\r");
+                logObject.Exception = logObject.Exception.Substring(0, index);
+                logObject.Response = "Failure";
+                logObject.TimeStamp = DateTime.Now;
+                addingobject.Add(logObject);
+            }
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe/Model/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Model
+{
+    public class Player
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        //public Guid Token { get; set; }
+        //public bool Status { get; set; }
+    }
+}
